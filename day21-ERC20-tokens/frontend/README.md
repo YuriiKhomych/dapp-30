173691cfb6cce979d6c9dcbea2603251bfc395f0
yuriikhomych <yuriykhomich@gmail.com>
2022-05-17 12:34:57 +0300

    Finish day 21

---
+++ day21-ERC20-tokens/frontend/README.md
@@ -0,0 +1,4 @@
+#Simple frontend for ERC20 tokens contract
+Run:
+1. `truffle develop` + `migrate --reset`
+2. `cd client & npm install & npm start`
---
+++ day21-ERC20-tokens/frontend/app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+//import React from 'react';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import App from './App';
+// import {createRoot} from "react-dom/client";
+// const root = createRoot(document.getElementById('root'));
+// root.render(<App />);
