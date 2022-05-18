2aa58d9aa102b0bcdddbab925bb896d25f0d7044
yuriikhomych <yuriykhomich@gmail.com>
2022-05-18 15:09:26 +0300

    Finish day 22

---
+++ day22-ICO/frontend/README.md
@@ -0,0 +1,4 @@
+#Simple frontend for ICO contract
+Run:
+1. `truffle develop` + `migrate --reset`
+2. `cd client & npm install & npm start`
---
+++ day22-ICO/frontend/app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import {createRoot} from "react-dom/client";
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
